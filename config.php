68e786528d4be35b4d07add4c5690332431a8486
Sagar <theseaisblueandgreen@gmail.com>
2021-10-18 19:28:56 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+$username = "pma";
+$password="";
+$server ="localhost";
+$db = "sparks_bank";
+
+
+$conn = mysqli_connect($server,$username,$password,$db);
+
+/*if($con)
+//echo "Connection successful";
+{
+    ?>
+    <script>
+        alert('Connection Successful');
+    </script>
+    <?php
+}
+else
+{
+    echo "No connection";
+    die("no connection".mysqli_connect_error());//exit
+}*/
+
+
+
+?>
